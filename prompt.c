b06812e558f90832ede9e24c07dff84bbc8d9277
skcoyle23 <52424103+skcoyle23@users.noreply.github.com>
2021-04-11 11:56:25 -0400

    Add files via upload

--- prompt.c
+++ prompt.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include "shell.h"
 
+/*
 void print_prompt1(void) {
 	fprintf(stderr, "$ ");
+} */
+
+void print_prompt1(void)
+{   
+    struct symtab_entry_s *entry = get_symtab_entry("PS1");
+
+    if(entry && entry->val)
+    {
+        fprintf(stderr, "%s", entry->val);
+    }
+    else
+    {
+        fprintf(stderr, "$ ");
+    }
 }
 
 void print_prompt2(void) {
